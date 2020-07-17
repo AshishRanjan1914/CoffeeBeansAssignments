b6fce62ddabf64dae3380fda12923bac4b3367ec
Ashish Ranjan <ashish.ranjan@blackstone.com>
2020-07-17 14:47:14 +0530

    added tests

--- src/test/java/mytests/BasicProgTest.java
+++ src/test/java/mytests/BasicProgTest.java
@@ -1,5 +1,8 @@
 package mytests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.Test;
 
 public class BasicProgTest {
@@ -7,12 +10,37 @@ public class BasicProgTest {
 	@Test
 	public void stringHavingWords() {
 		
+		String test = "Bangalore is in India";
+		
+		String[] splittedtxt = test.split("\\s+");
+		
+		
+		for (int i = 0; i < splittedtxt.length; i++) {
+			Map<Character,Integer> countMap = new HashMap<Character, Integer>();
+			
+			char[] strArray = splittedtxt[i].toCharArray();
+			for (char c : strArray) { 
+				if (countMap.containsKey(c)) {
+					countMap.put(c, countMap.get(c) + 1);
+				} else {
+					countMap.put(c, 1);
+				}
+			}
+			 System.out.println("For Word "+splittedtxt[i]);
+			for (Map.Entry entry : countMap.entrySet()) { 
+	            System.out.println(entry.getKey() + " " + entry.getValue()); 
+	        }
+			
+		}
 		
 		
 	}
 	
 	@Test
 	public void findNumber() {
+		
+		int x;
+		
 		
 	}
 
--- src/test/java/mytests/ClearTripTest.java
+++ src/test/java/mytests/ClearTripTest.java
@@ -1,14 +1,68 @@
 package mytests;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import base.MainBaseClass;
+import sun.misc.JavaSecurityAccess;
 
 public class ClearTripTest extends MainBaseClass {
+
+	WebDriverWait wait;
+
+	private void JSSetValueElement(WebElement ele, String longstring) {
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].setAttribute('value', '" + longstring +"')", ele);
+	}
+
+	private void JSClickElement(WebElement ele) {
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", ele);
+	}
 	
+	private String getTodayDate(String args) {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); 
+		Date date = new Date();
+		if (args.equalsIgnoreCase("add")) {
+			long ltime=date.getTime()+10*24*60*60*1000;
+			date = new Date(ltime);
+		}
+		
+		return formatter.format(date);
+		
+	}
+
 	@Test
 	public void clearTripTest() {
 		driver.get(configProp.getProperty("ClearTrip"));
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(orProp.getProperty("FromTag"))));
+
+		driver.findElement(By.id("RoundTrip")).click();
+		JSClickElement(driver.findElement(By.id("RoundTrip")));
+		driver.findElement(By.id(orProp.getProperty("FromTag"))).click();
+		JSSetValueElement(driver.findElement(By.id("From")), "Del");
+		
+		driver.findElement(By.id(orProp.getProperty("ToTag"))).click();
+		JSSetValueElement(driver.findElement(By.id("To")), "BOM");
+		
+		driver.findElement(By.id(orProp.getProperty("DepartDate"))).click();
+		JSSetValueElement(driver.findElement(By.id("FromDate")), getTodayDate(""));
+		
+		driver.findElement(By.id(orProp.getProperty("DepartDate"))).click();
+		JSSetValueElement(driver.findElement(By.id("FromDate")), getTodayDate(""));
+		
+		JSClickElement(driver.findElement(By.id("SearchBtn")));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[.='Details']")));
+
 	}
 
 }
